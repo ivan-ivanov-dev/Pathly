c5633597dded0e4512c8159d75894c67e009c19a
MagMen1008 <darkkey7712@gmail.com>
2026-02-05 20:01:55 +0200

    Add the Index view for the dashboard and a navbar for it

---
+++ MicroTaskTracker/Models/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroTaskTracker.Models.ViewModels.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public DashboardStatsViewModel Stats { get; set; } = null!;
+        public DashboardFocusListsViewModel FocusLists { get; set; } = null!;
+    }
+}
