0c434ab65d7ed58cab5441c8ebc8b3ab84d4dab1
MagMen1008 <darkkey7712@gmail.com>
2026-02-12 17:22:03 +0200

    Add Tag DBModel

---
+++ MicroTaskTracker/Models/DBModels/Tag.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MicroTaskTracker.Models.DBModels
+{
+    public class Tag
+    {
+        
+        
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<TaskTag> TaskTags { get; set; } = new List<TaskTag>();
+    }
+}
