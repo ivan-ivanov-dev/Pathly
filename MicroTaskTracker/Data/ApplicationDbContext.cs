97c0424372698f44aa3e3feb112a73fce917c385
MagMen1008 <darkkey7712@gmail.com>
2026-02-13 14:03:07 +0200

    Add the Roadmap set to db context and configure realationships correctly

--- MicroTaskTracker/Data/ApplicationDbContext.cs
+++ MicroTaskTracker/Data/ApplicationDbContext.cs
@@ -15,11 +15,13 @@ namespace MicroTaskTracker.Data
         public DbSet<TaskTag> TaskTags => Set<TaskTag>();
         public DbSet<Goal> Goals => Set<Goal>();
         public DbSet<ActionItem> Actions => Set<ActionItem>();
+        public DbSet<Roadmap> Roadmaps => Set<Roadmap>();
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            // TaskTag (many-to-many)
             builder.Entity<TaskTag>()
                 .HasKey(tt => new { tt.TaskId, tt.TagId });
 
@@ -33,30 +35,48 @@ namespace MicroTaskTracker.Data
                 .WithMany(t => t.TaskTags)
                 .HasForeignKey(tt => tt.TagId);
 
+            // Tag -> User
             builder.Entity<Tag>()
                 .HasOne(t => t.User)
                 .WithMany()
                 .HasForeignKey(t => t.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Goal -> User
             builder.Entity<Goal>()
                 .HasOne(g => g.User)
-                .WithMany() // if you don’t need a collection in ApplicationUser
+                .WithMany()
                 .HasForeignKey(g => g.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            builder.Entity<Goal>()
-                .HasMany(g => g.Actions)
-                .WithOne(a => a.Goal)
-                .HasForeignKey(a => a.GoalId)
+            // Roadmap -> User
+            builder.Entity<Roadmap>()
+                .HasOne(r => r.User)
+                .WithMany()
+                .HasForeignKey(r => r.UserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Roadmap -> Goal (ONE-TO-ONE)
+            builder.Entity<Roadmap>()
+                .HasOne(r => r.Goal)
+                .WithOne()
+                .HasForeignKey<Roadmap>(r => r.GoalId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Roadmap -> Actions (ONE-TO-MANY)
+            builder.Entity<Roadmap>()
+                .HasMany(r => r.Actions)
+                .WithOne(a => a.Roadmap)
+                .HasForeignKey(a => a.RoadmapId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Action -> Tasks (ONE-TO-MANY, nullable on Task)
             builder.Entity<ActionItem>()
                 .HasMany(a => a.Tasks)
                 .WithOne(t => t.Action)
                 .HasForeignKey(t => t.ActionId)
                 .OnDelete(DeleteBehavior.SetNull);
-
         }
+
     }
 }
