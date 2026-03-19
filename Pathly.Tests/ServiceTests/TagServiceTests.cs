c102cc1d1524f66f5a8b31c12c6c55a59f6c7d96
MagMen1008 <darkkey7712@gmail.com>
2026-03-19 22:01:46 +0200

    Add ServiceTests - Tag Service Tests

---
+++ Pathly.Tests/ServiceTests/TagServiceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Pathly.Tests;
+
+public class TagServiceTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
