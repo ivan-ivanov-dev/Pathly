3bd325a48b084deb05e49755449965116a679ed1
MagMen1008 <darkkey7712@gmail.com>
2026-03-19 17:03:58 +0200

    Add Seeding for TaskTag - connect tags to tasks

--- Pathly.Data/Seeding/Configurations/TagConfiguration.cs
+++ Pathly.Data/Seeding/Configurations/TagConfiguration.cs
@@ -17,14 +17,14 @@ namespace Pathly.Data.Seeding.Configurations
             builder.HasData(
                 new Tag { Id = 1, Name = "Work", UserId = userId },
                 new Tag { Id = 2, Name = "Personal", UserId = userId },
-                new Tag { Id = 2, Name = "C#", UserId = userId },
-                new Tag { Id = 2, Name = "Gym", UserId = userId },
-                new Tag { Id = 2, Name = "Frontend", UserId = userId },
-                new Tag { Id = 2, Name = "Testing", UserId = userId },
-                new Tag { Id = 2, Name = "Learning", UserId = userId },
-                new Tag { Id = 2, Name = "Soft Skill", UserId = userId },
-                new Tag { Id = 2, Name = "School", UserId = userId },
-                new Tag { Id = 3, Name = "Urgent", UserId = userId }
+                new Tag { Id = 3, Name = "C#", UserId = userId },
+                new Tag { Id = 4, Name = "Gym", UserId = userId },
+                new Tag { Id = 5, Name = "Frontend", UserId = userId },
+                new Tag { Id = 6, Name = "Testing", UserId = userId },
+                new Tag { Id = 7, Name = "Learning", UserId = userId },
+                new Tag { Id = 8, Name = "Soft Skill", UserId = userId },
+                new Tag { Id = 9, Name = "School", UserId = userId },
+                new Tag { Id = 10, Name = "Urgent", UserId = userId }
             );
         }
     }
