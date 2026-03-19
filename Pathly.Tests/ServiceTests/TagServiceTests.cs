0d53f5d558f0aba76f35004f944f7e199502ced3
MagMen1008 <darkkey7712@gmail.com>
2026-03-19 22:15:11 +0200

    Set up TagService Tests

--- Pathly.Tests/ServiceTests/TagServiceTests.cs
+++ Pathly.Tests/ServiceTests/TagServiceTests.cs
@@ -1,15 +1,46 @@
-﻿namespace Pathly.Tests;
+﻿using Moq;
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using Pathly.Data;
+using Pathly.DataModels;
+using Pathly.Services.Implementation;
+using AutoMapper;
+using Pathly.ViewModels.Tags;
+namespace Pathly.Tests;
 
+[TestFixture]
 public class TagServiceTests
 {
+    private ApplicationDbContext _context;
+    private Mock<IMapper> _mapperMock;
+    private TagService _tagService;
+
     [SetUp]
     public void Setup()
     {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+        _context = new ApplicationDbContext(options);
+        _mapperMock = new Mock<IMapper>();
+
+        // 2. Инициализираме сървиса
+        _tagService = new TagService(_mapperMock.Object,_context);
     }
 
     [Test]
     public void Test1()
     {
         Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (_context != null)
+        {
+            _context.Dispose();
+        }
     }
 }
--- Pathly.ViewModels/Tags/TagViewModel.cs
+++ Pathly.ViewModels/Tags/TagViewModel.cs
@@ -5,6 +5,7 @@ namespace Pathly.ViewModels.Tags
 {
     public class TagViewModel
     {
+        public int Id { get; set; }
         [Required(ErrorMessage = ErrorMessages.TagNameIsRequired)]
         [StringLength(ValidationConstants.MaxTagNameLength, ErrorMessage = ErrorMessages.TagNameCannotExceed30Characters)]
         public string Name { get; set; } = null!;
