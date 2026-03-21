c7d4e42c4881da2f3b56193b45d088f0098e5885
MagMen1008 <darkkey7712@gmail.com>
2026-03-21 11:17:05 +0200

    Map TaskEditViewModel -> TaskItem

--- Pathly.Services/Mappings/MappingProfile.cs
+++ Pathly.Services/Mappings/MappingProfile.cs
@@ -122,6 +122,16 @@ namespace Pathly.Services.Mappings
                 .ForMember(dest => dest.SelectedTagIds, opt => opt.Ignore())
                 .ForMember(dest => dest.AvailableTags, opt => opt.Ignore());
 
+            // В MappingProfile.cs
+
+            CreateMap<TaskEditViewModel, TaskItem>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
+                .ForMember(dest => dest.TaskTags, opt => opt.Ignore())
+                .ForMember(dest => dest.Action, opt => opt.Ignore());
+
             CreateMap<TaskDetailsViewModel, TaskEditViewModel>()
                 .ForMember(dest => dest.SelectedTagIds, opt => opt.Ignore())
                 .ForMember(dest => dest.AvailableTags, opt => opt.Ignore());
