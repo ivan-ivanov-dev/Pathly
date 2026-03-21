20155b4262bfefbd51bb370db70af1d28c8c2193
MagMen1008 <darkkey7712@gmail.com>
2026-03-21 14:28:23 +0200

    Fix Mapping Tests Error

--- Pathly.Tests/Mapping/MappingTests.cs
+++ Pathly.Tests/Mapping/MappingTests.cs
@@ -12,7 +12,6 @@ public class MappingTests
         // Arrange
         var config = new MapperConfiguration(cfg =>
         {
-            //add profiles here
             cfg.AddProfile<MappingProfile>();
         });
 
