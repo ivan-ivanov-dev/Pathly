fa653a824d8c044316ed4241855aefaf4ce625a1
MagMen1008 <darkkey7712@gmail.com>
2026-03-25 00:00:20 +0200

    Fix Admin mapping so it maps to DashboardController and not HomeController

--- Pathly.Web/Program.cs
+++ Pathly.Web/Program.cs
@@ -72,7 +72,7 @@ namespace Pathly.Web
 
             app.MapControllerRoute(
                  name: "areas",
-                 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                 pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
             app.MapControllerRoute(
                 name: "default",
