2f6915e74677f998a8dec9184b6cf93446270e22
MagMen1008 <darkkey7712@gmail.com>
2026-03-24 23:22:37 +0200

    Add RoleConfiguration

---
+++ Pathly.Data/Seeding/Configurations/RoleConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pathly.GCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.Data.Seeding.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Id = SeedConstants.AdminRoleId,
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                },
+                new IdentityRole
+                {
+                    Id = SeedConstants.DemoRoleId,
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            );
+        }
+    }
+}
