039fecdaeaadbc53fa0f57d294558c4b087f18b8
MagMen1008 <darkkey7712@gmail.com>
2026-03-25 00:01:13 +0200

    Add AdminBaseController and a DashboardController which inherits the base controller

---
+++ Pathly.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pathly.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public abstract class AdminBaseController : Controller
+    {
+    }
+}
