f06d9972500f04768345f7fdee971371195a765f
MagMen1008 <darkkey7712@gmail.com>
2026-03-24 21:56:07 +0200

    Add ErrorController to redirect to custom error pages and remove the Error action in HomeController

---
+++ Pathly.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pathly.Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        [Route("Errors/Error404")]
+        public IActionResult Error404()
+        {
+            return View();
+        }
+
+        [Route("Errors/Error500")]
+        public IActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
