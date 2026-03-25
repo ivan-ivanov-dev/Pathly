fcfac9df22d193dab06200663e909d4e3fcde0cf
MagMen1008 <darkkey7712@gmail.com>
2026-03-25 09:19:28 +0200

    Create a DTO to map the passwords

---
+++ Pathly.DataModels/SeedSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.DataModels
+{
+    public class SeedSettings
+    {
+        public string AdminPassword { get; set; } = null!;
+        public string TestUserPassword { get; set; } = null!;
+    }
+}
