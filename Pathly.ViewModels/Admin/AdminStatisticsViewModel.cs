d898655bfcf5562a2a0a99ba88ab2e6fceb81ddc
MagMen1008 <darkkey7712@gmail.com>
2026-03-25 21:37:17 +0200

    Add AdminStatisticsViewModel to save total users, total goals, completed tasks and the new usesr the last 24 hours

---
+++ Pathly.ViewModels/Admin/AdminStatisticsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.ViewModels.Admin
+{
+    public class AdminStatisticsViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalGoals { get; set; }
+        public int CompletedTasks { get; set; }
+        public int NewUsersLast24Hours { get; set; }
+    }
+}
