cec269d8f6c70dc9f097f2b8b968de31a7e32fbd
MagMen1008 <darkkey7712@gmail.com>
2026-03-25 09:57:22 +0200

    Add UserListViewModel

---
+++ Pathly.ViewModels/Admin/UserListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.ViewModels.Admin
+{
+    public class UserListViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+
+        public IEnumerable<string> Roles { get; set; } = new List<string>();
+    }
+}
