f5457dab5581b833a6060e1e8b2e686a38418e35
MagMen1008 <darkkey7712@gmail.com>
2026-03-26 23:07:44 +0200

    Modify GetTagsAsync so that it accepts a searchString and helps with filtering tags by name

--- Pathly.Services/Contracts/ITagService.cs
+++ Pathly.Services/Contracts/ITagService.cs
@@ -5,7 +5,7 @@ namespace Pathly.Services.Contracts
 {
     public interface ITagService
     {
-        public Task<IEnumerable<TagViewModel>> GetUserTagsAsync(string userId); 
+        public Task<IEnumerable<TagViewModel>> GetUserTagsAsync(string userId, string searchString = null); 
         public Task CreateTagAsync(string name, string userId); 
         public Task<bool> DeleteTagAsync(int id, string userId);
     }
