98d7beec9b2a90a7c23d3bb8f1a1ee22930e5585
MagMen1008 <darkkey7712@gmail.com>
2026-03-27 00:55:29 +0200

    Add SettingsViewModel

---
+++ Pathly.ViewModels/Settings/SettingsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Pathly.GCommon;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.ViewModels.Settings
+{
+    public class SettingsViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string? CurrentPassword { get; set; }
+
+        [MinLength(ValidationConstants.MinPasswordLength, ErrorMessage = ErrorMessages.PasswordMustBeAtLeast6CharactersLong)]
+        [MaxLength(ValidationConstants.MaxPasswordLength, ErrorMessage = ErrorMessages.PasswordCannotExceed100Characters)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = ErrorMessages.PasswordsDoNotMatch)]
+        public string? ConfirmPassword { get; set; }
+    }
+}
