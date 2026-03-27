17a35f438edc882886f5815377683590eef88977
MagMen1008 <darkkey7712@gmail.com>
2026-03-27 11:15:06 +0200

    Use Settings service DeleteUserData method to create an action for deleting Users

--- Pathly.Web/Controllers/SettingsController.cs
+++ Pathly.Web/Controllers/SettingsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Pathly.DataModels;
+using Pathly.Services.Contracts;
 using Pathly.ViewModels.Settings;
 
 namespace Pathly.Web.Controllers
@@ -9,11 +10,13 @@ namespace Pathly.Web.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ISettingsService _settingsService;
 
-        public SettingsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public SettingsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ISettingsService settingsService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _settingsService = settingsService;
         }
 
         [HttpGet]
@@ -80,6 +83,30 @@ namespace Pathly.Web.Controllers
             
             }
             return View("Index", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteAccount()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) return NotFound();
+
+            var dataWiped = await _settingsService.DeleteUserDataAsync(user.Id);
+            if (!dataWiped)
+            {
+                TempData["ErrorMessage"] = "Could not delete your data. Please try again.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
