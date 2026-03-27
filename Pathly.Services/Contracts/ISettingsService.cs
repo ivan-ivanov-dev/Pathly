368c40e0d13be3186963d88dfcfb351105fd626f
MagMen1008 <darkkey7712@gmail.com>
2026-03-27 10:55:06 +0200

    Add ISettingService with method for Deleting user data

---
+++ Pathly.Services/Contracts/ISettingsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.Services.Contracts
+{
+    public interface ISettingsService
+    {
+        Task<bool> DeleteUserDataAsync(string userId);
+    }
+}
