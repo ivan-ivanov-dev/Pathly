d6cbbfe8a03e0e38e328dc75b4a452e71e79de4a
MagMen1008 <darkkey7712@gmail.com>
2026-03-29 21:15:18 +0300

    Add and implement BlobService with methods for uploading a file and one for generating temporary SAS tokens which allow read only access

---
+++ Pathly.Services/Contracts/IBlobService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.Services.Contracts
+{
+    public interface IBlobService
+    {
+        Task<string> UploadFileAsync(IFormFile file);
+        string GetReadOnlyLink(string blobName);
+    }
+}
---
+++ Pathly.Services/Implementation/BlobService.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Pathly.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.Services.Implementation
+{
+    public class BlobService : IBlobService
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerName;
+
+        public BlobService(IConfiguration configuration)
+        {
+            _blobServiceClient = new BlobServiceClient(configuration["AzureStorage:ConnectionString"]);
+            _containerName = configuration["AzureStorage:ContainerName"];
+        }
+
+        public string GetReadOnlyLink(string blobName)
+        {
+            if (string.IsNullOrEmpty(blobName))
+            {
+                return null;
+            }
+
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            // Generate a SAS token for read-only access - valid for 1h
+            if (blobClient.CanGenerateSasUri)
+            {
+                BlobSasBuilder sasBuilder = new BlobSasBuilder()
+                {
+                    BlobContainerName = _containerName,
+                    BlobName = blobName,
+                    Resource = "b",
+                    ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
+                };
+                sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+                return blobClient.GenerateSasUri(sasBuilder).ToString();
+            }
+            return null;
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(_containerName);
+            await containerClient.CreateIfNotExistsAsync();
+
+            string blobName = $"{Guid.NewGuid()}_{file.FileName}";
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            using var stream = file.OpenReadStream();
+            await blobClient.UploadAsync(stream, true);
+
+            return blobName;
+        }
+    }
+}
