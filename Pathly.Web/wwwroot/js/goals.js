8e8ae7c8da82aa9157c22155955e320cb83d47c1
MagMen1008 <darkkey7712@gmail.com>
2026-04-01 21:04:28 +0300

    Add sweet alert 2 for a confirmation pop up when user tries to delete a goal

---
+++ Pathly.Web/wwwroot/js/goals.js
@@ -0,0 +1,30 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const deleteForms = document.querySelectorAll('.delete-goal-form');
+
+    deleteForms.forEach(form => {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            Swal.fire({
+                title: 'Delete This Goal?',
+                text: "This will remove the goal from your active list. Do you want to proceed?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#0F4C5C', 
+                cancelButtonColor: '#6c757d',
+                confirmButtonText: 'Yes, delete it',
+                cancelButtonText: 'No,keep it',
+                background: '#ffffff',
+                borderRadius: '15px',
+                customClass: {
+                    title: 'fw-bold',
+                    popup: 'shadow-sm border-0'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    form.submit();
+                }
+            });
+        });
+    });
+});
