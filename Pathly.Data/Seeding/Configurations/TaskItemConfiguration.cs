fb5ad4fe6c8472318c6dd50474424049fce95b4b
MagMen1008 <darkkey7712@gmail.com>
2026-04-03 18:51:02 +0300

    Update Tasks Seeding to include new properties for Status and Position and aslo set IsCompleted bool accordingly

--- Pathly.Data/Seeding/Configurations/TaskItemConfiguration.cs
+++ Pathly.Data/Seeding/Configurations/TaskItemConfiguration.cs
@@ -2,49 +2,68 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Pathly.DataModels;
 using Pathly.GCommon;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pathly.Data.Seeding.Configurations
 {
     public class TaskItemConfiguration : IEntityTypeConfiguration<TaskItem>
     {
         string userId = SeedConstants.DemoUserId;
+
         public void Configure(EntityTypeBuilder<TaskItem> builder)
         {
             var tasks = new List<TaskItem>();
             int taskId = 1;
+
+            int todoPos = 0;
+            int progressPos = 0;
+            int donePos = 0;
+
+            //Linked tasks logic
             for (int actionId = 1; actionId <= 9; actionId++)
             {
                 for (int j = 1; j <= 3; j++)
                 {
+                    var status = (DataModels.TaskStatus)((taskId % 3) + 1);
+
+                    bool isDone = status == DataModels.TaskStatus.Done;
+
+                    int position = status switch
+                    {
+                        DataModels.TaskStatus.Todo => todoPos++,
+                        DataModels.TaskStatus.InProgress => progressPos++,
+                        _ => donePos++
+                    };
+
                     tasks.Add(new TaskItem
                     {
                         Id = taskId++,
                         Title = $"Task {j} for Action {actionId}",
-                        Description = "Seed description",
-                        Priority = (TaskPriority)(taskId % 4 + 1), // Different priority levels
-                        IsCompleted = taskId % 2 == 0,
-                        CreatedOn = DateTime.Now.AddDays(-10),
-                        DueDate = DateTime.Now.AddDays(5),
+                        Description = "Strategic seed description",
+                        Priority = (TaskPriority)(taskId % 4 + 1),
+                        Status = status,
+                        IsCompleted = isDone,
+                        Position = position,
+                        CreatedOn = DateTime.UtcNow.AddDays(-10),
+                        DueDate = DateTime.UtcNow.AddDays(5),
                         ActionId = actionId,
                         UserId = userId
                     });
                 }
             }
+
+            // General tasks logic
             for (int i = 1; i <= 5; i++)
             {
                 tasks.Add(new TaskItem
                 {
                     Id = taskId++,
                     Title = $"General Task {i}",
-                    Description = "Unlinked task description",
+                    Description = "Unlinked high-level objective",
                     Priority = TaskPriority.Medium,
+                    Status = DataModels.TaskStatus.Todo,
                     IsCompleted = false,
-                    CreatedOn = DateTime.Now,
+                    Position = todoPos++,
+                    CreatedOn = DateTime.UtcNow,
                     UserId = userId
                 });
             }
