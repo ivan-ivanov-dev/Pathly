81df9aa86b7905382078cd2d7d8546694efb5221
MagMen1008 <darkkey7712@gmail.com>
2026-04-03 22:47:48 +0300

    Map Event and EventFormViewModel and Event and Event and EventCalendarViewModel

--- Pathly.Services/Mappings/MappingProfile.cs
+++ Pathly.Services/Mappings/MappingProfile.cs
@@ -2,6 +2,7 @@
 using Pathly.DataModels;
 using Pathly.ViewModels;
 using Pathly.ViewModels.Dashboard;
+using Pathly.ViewModels.Event;
 using Pathly.ViewModels.Goals;
 using Pathly.ViewModels.Roadmaps;
 using Pathly.ViewModels.Tags;
@@ -170,6 +171,29 @@ namespace Pathly.Services.Mappings
 
             // --- DASHBOARD ---
             CreateMap<DashboardFocusListsViewModel, DashboardFocusListsViewModel>();
+
+            // --- EVENTS ---
+            CreateMap<Event, EventCalendarViewModel>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Title, opt => opt.Ignore())
+                .ForMember(dest => dest.Description, opt => opt.Ignore())
+                .ForMember(dest => dest.Start, opt => opt.MapFrom(src => src.Start.ToString("yyyy-MM-ddTHH:mm:ss")))
+                .ForMember(dest => dest.End, opt => opt.MapFrom(src => src.End.ToString("yyyy-MM-ddTHH:mm:ss")))
+                .ForMember(dest => dest.Color, opt => opt.MapFrom(src => src.ColorHex))
+                .ForMember(dest => dest.AllDay, opt => opt.MapFrom(src => src.IsAllDay));
+
+            CreateMap<Event, EventFormViewModel>()
+                .ForMember(dest => dest.AvailableTasks, opt => opt.Ignore())
+                .ForMember(dest => dest.AvailableGoals, opt => opt.Ignore())
+                .ReverseMap()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
+                .ForMember(dest => dest.Task, opt => opt.Ignore())
+                .ForMember(dest => dest.TaskId, opt => opt.Ignore())
+                .ForMember(dest => dest.Goal, opt => opt.Ignore())
+                .ForMember(dest => dest.GoalId, opt => opt.Ignore());
         }
     }
 }
