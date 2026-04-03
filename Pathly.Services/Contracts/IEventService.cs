93b9ede4665d4eaeb7eefa5d10832ab5972eee2c
MagMen1008 <darkkey7712@gmail.com>
2026-04-03 23:11:37 +0300

    Add and Implement IEventService

---
+++ Pathly.Services/Contracts/IEventService.cs
@@ -0,0 +1,21 @@
+﻿using Pathly.ViewModels.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.Services.Contracts
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<EventCalendarViewModel>> GetAllForCalendarAsync(string userId);
+        Task<EventFormViewModel?> GetForEditAsync(int id, string userId);
+        Task CreateAsync(EventFormViewModel model, string userId);
+        Task UpdateAsync(EventFormViewModel model, string userId);
+        Task DeleteAsync(int id, string userId);
+
+        // UI Helper
+        Task<EventFormViewModel> PrepareFormModelAsync(string userId);
+    }
+}
