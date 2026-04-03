23b32b8166e6ca2ca59b1890356c567a094d8093
MagMen1008 <darkkey7712@gmail.com>
2026-04-03 20:38:06 +0300

    Add a folder for hubs and add KanbanHub

---
+++ Pathly.Web/Hubs/KanbanHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Pathly.Web.Hubs
+{
+    public class KanbanHub: Hub
+    {
+        // This method will be called from JavaScript when a task drag ends in the kanban board
+        public async Task NotifyTaskMoved(string taskId, int newStatus, int newPosition)
+        {
+            await Clients.Others.SendAsync("ReceiveTaskMove", taskId, newStatus, newPosition);
+        }
+    }
+}
