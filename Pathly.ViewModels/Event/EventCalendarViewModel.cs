25e599f7f96d506b8b498130780c61d0eb46a902
MagMen1008 <darkkey7712@gmail.com>
2026-04-03 22:07:01 +0300

    Add VMs for Events: -EventCalendarViewModel for the calendar index view -EventFormViewModel for the calendar create/edit partials

---
+++ Pathly.ViewModels/Event/EventCalendarViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathly.ViewModels.Event
+{
+    public class EventCalendarViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Start { get; set; } = null!; // String format for JS
+        public string End { get; set; } = null!; // String format for JS
+        public bool AllDay { get; set; }
+        public string Color { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
